a8cddc18de909e0ac7b07b1d0fcf6f167c6114ed
Taylor <tbnelson01@gmail.com>
2022-10-10 14:49:53 -0400

    showing board

--- mystery_word.py
+++ mystery_word.py
@@ -1,4 +1,3 @@
-    
 def create_board(word):
     board_list = []
     for letter in word:
@@ -6,28 +5,35 @@ def create_board(word):
     print(f"Here is your word to guess: {' '.join(board_list)}")
     return board_list
 
-#def show_board(word):
-    #TODO display
-
+def show_board(word, board, correct_list):
+    for letter in correct_list:
+        for idx in range(len(word)):
+            if word[idx] == letter:
+                board[idx] = letter
+    print(" ".join(board))
 
 def get_user_guess():
     guess = input("Guess your letter: ")
-    print(guess)
     return guess
 
 def play_game():
     # computer picks a word at random from the list
     # start with one word
     # TODO pick word from list
-    word_to_guess = 'dream'
+    word_to_guess = 'dream'.upper()
 
     # let user know length of secret word. make the board showing blanks for letters.
     game_board = create_board(word_to_guess)
     correct_guesses = []
     incorrect_guesses = []
     #show_board(word_to_guess)
-    new_guess = get_user_guess()
-
+    new_guess = get_user_guess().upper()
+    if new_guess in word_to_guess:
+        correct_guesses.append(new_guess)
+    else:
+        incorrect_guesses.append(new_guess)
+    show_board(word_to_guess, game_board, correct_guesses)
+    print(f"Incorrect letters: {' '.join(incorrect_guesses)}")
 
 
 
