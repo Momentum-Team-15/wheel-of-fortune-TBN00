e0ae38c7444b482a754d693f5b798e15f4329f38
Taylor <tbnelson01@gmail.com>
2022-10-10 11:39:26 -0400

    Mistawee

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,35 @@
+    
+def create_board(word):
+    board_list = []
+    for letter in word:
+        board_list.append("_")
+    print(f"Here is your word to guess: {' '.join(board_list)}")
+    return board_list
+
+#def show_board(word):
+    #TODO display
+
+
+def get_user_guess():
+    guess = input("Guess your letter: ")
+    print(guess)
+    return guess
+
 def play_game():
-    pass
+    # computer picks a word at random from the list
+    # start with one word
+    # TODO pick word from list
+    word_to_guess = 'dream'
+
+    # let user know length of secret word. make the board showing blanks for letters.
+    game_board = create_board(word_to_guess)
+    correct_guesses = []
+    incorrect_guesses = []
+    #show_board(word_to_guess)
+    new_guess = get_user_guess()
+
+
+
 
 
 if __name__ == "__main__":
